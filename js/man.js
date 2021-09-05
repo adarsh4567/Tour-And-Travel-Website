f89c4da5e6fb568fc21dedb340a6896cc5f3cc33
Adarsh <adarsh800251@gmail.com>
2021-09-05 11:34:21 +0530

    tour website added

---
+++ js/man.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        
+        
+    });
+    AOS.init();
+
+});
+
